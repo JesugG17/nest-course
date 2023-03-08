90dde137a66d60fefd5a0726209ff6e86b6cba6c
Jesus Gastelum <gastelumtec21@gmail.com>
2023-03-07 22:41:36 -0700

    logica basica para el servicio

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,21 @@
 import { Controller } from '@nestjs/common';
 import { Get, Param } from '@nestjs/common/decorators';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    private cars = ['Toyota', 'Honda', 'Jeep']; 
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()
     getAllCars() {
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
-    getCarById(@Param('id') id: number) {
-        return { car: this.cars[id] };
+    getCarById(@Param('id') id: string) {
+        return this.carsService.findOneById(Number(id));
     }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        },
+        {
+            id: 2,
+            brand: 'Jeep',
+            model: 'Corolla'
+        },
+        {
+            id: 3,
+            brand: 'Honda',
+            model: 'Corolla'
+        }
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById(id: number) {
+        return this.cars.find( car => car.id === Number(id) );
+    }
+}
