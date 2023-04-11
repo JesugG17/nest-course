4f4ad16d69ec615b8798b5f7f09831e5055fba20
Jesus Gastelum <gastelumtec21@gmail.com>
2023-04-10 21:45:26 -0700

    pokemon resource añadido, imagen de mongo en docker hecha

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,7 +1,16 @@
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
+  ],
 })
 export class AppModule {}
